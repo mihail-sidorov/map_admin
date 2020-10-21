701da8dc55daba47614540a44f62a8b272e66617
fruit765 <megafpykt@ya.ru>
2020-10-21 11:35:38 +0300

    logging

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,13 @@
 'use strict'
 
+const fs = require("fs")
+const access = fs.createWriteStream('log')
+process.stdout.write = process.stderr.write = access.write.bind(access)
+
 const express = require("express")
 const app = express()
 const port = require("../serverConfig").server.port
-
+4454tdfgf
 require("./middlewares/useMiddlewares")(app)
 require("./routes")(app)
 
